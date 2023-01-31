8ffa5306b4076610e8f61845c1a9405be322cdee
Kada's PC <ada2160594@maricopa.edu>
2023-01-30 20:42:46 -0700

    idk

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <html lang="en">
     <h1>
         <?php
+        date_default_timezone_set('America/Phoenix');
+            echo "Today is: " . date("Y/m/d") . "<br>";
+            echo "The time is: " . date("h:i:sa") . "<br>";
+            echo "In one week it will be: " . date('Y-m-d H:i:s', strtotime("+7 day \n")) . "<br>";
+            echo "The semester will end in: Error: User does not know when the semester will end :<" . "<br>";
 
-            echo "Today is " . date("Y/m/d \n");
-            echo "The time is " . date("h:i:sa \n");
-            echo date('Y-m-d H:i:s', strtotime("+7 day \n"));
-
-            echo("I really have no idea how im supposed to upload this! \n")
+            echo("I really have no idea how im supposed to upload this to GIT!!") . "<br>";
 
         ?>
     </h1>
