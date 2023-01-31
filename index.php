672ecc5f139a4485a1b8a1ed25f74d798e7084d6
Kada's PC <ada2160594@maricopa.edu>
2023-01-30 18:12:21 -0700

    idk

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <html lang="en">
     <h1>
         <?php
-            date_default_timezone_set("America/Arizona");
 
-            echo "Today is " . date("Y/m/d") . "<br>";
-            echo "The time is " . date("h:i:sa") . "<br>";
-            echo date('Y-m-d H:i:s', strtotime("+7 day"));
+            echo "Today is " . date("Y/m/d \n");
+            echo "The time is " . date("h:i:sa \n");
+            echo date('Y-m-d H:i:s', strtotime("+7 day \n"));
 
-            echo('I really have no idea how im supposed to upload this!')
+            echo("I really have no idea how im supposed to upload this! \n")
 
         ?>
     </h1>
