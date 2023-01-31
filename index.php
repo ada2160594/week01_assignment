2579bee8c5edd0e0ee6fd557702dd7819c1240aa
Kada's PC <ada2160594@maricopa.edu>
2023-01-30 21:54:48 -0700

    week02 date test

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <html lang="en">
     <h1>
         <?php
-        date_default_timezone_set('America/Phoenix');
+            date_default_timezone_set('America/Phoenix');
             echo "Today is: " . date("Y/m/d") . "<br>";
             echo "The time is: " . date("h:i:sa") . "<br>";
-            echo "In one week it will be: " . date('Y-m-d H:i:s', strtotime("+7 day \n")) . "<br>";
-            echo "The semester will end in: Error: User does not know when the semester will end :<" . "<br>";
+            echo "In one week it will be: " . date('Y/m/d H:i:sa', strtotime("+7 day \n")) . "<br>";
 
-            echo("I really have no idea how im supposed to upload this to GIT!!") . "<br>";
+            $d1 = strtotime("01/01/2023");
+            $d2 = ceil(($d1 - time()) / 60 / 60 / 24);
+            echo "January 1st of 2023 was " . $d2 . "." . "<br>";
+
+            echo "Weeks until semester end: " . "<br>";
+
+        echo("I really have no idea how im supposed to upload this to GIT!! But I have tried!!!") . "<br>";
 
         ?>
     </h1>
