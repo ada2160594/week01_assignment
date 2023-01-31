ce00a91464c1d3955e172db295df421ba28e1633
Adam W <123705802+ada2160594@users.noreply.github.com>
2023-01-30 22:16:52 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <html lang="en">
     <h1>
         <?php
+            echo "Name: Adam Willoughby" . "<br>";
             date_default_timezone_set('America/Phoenix');
-            echo "Today is: " . date("Y/m/d") . "<br>";
+            echo "Today is: " . date("Y/n/d") . "<br>";
             echo "The time is: " . date("h:i:sa") . "<br>";
-            echo "In one week it will be: " . date('Y/m/d H:i:sa', strtotime("+7 day \n")) . "<br>";
+            echo "In one week it will be: " . date('Y/n/d', strtotime("+7 day \n")) . "<br>";
 
             $d1 = strtotime("01/01/2023");
             $d2 = ceil(($d1 - time()) / 60 / 60 / 24);
-            echo "January 1st of 2023 was " . $d2 . "." . "<br>";
+            echo "January 1st of 2023 was " . $d2 . "<br>";
 
             echo "Weeks until semester end: " . "<br>";
 
