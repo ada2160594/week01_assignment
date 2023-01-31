2e8c548c6b0ff8d540535bd55be7b11e41011854
Kada's PC <ada2160594@maricopa.edu>
2023-01-30 17:55:34 -0700

    idk

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
             echo "The time is " . date("h:i:sa") . "<br>";
             echo date('Y-m-d H:i:s', strtotime("+7 day"));
 
+            echo('I really have no idea how im supposed to upload this!')
+
         ?>
     </h1>
 </html>
